73bcd85d65c12d1d2cd1f5dc7c8289ba37527734
ctg2a <popovanatalya472@gmail.com>
2024-11-15 03:12:18 +0300

    2-7 tasks

--- froggs.py
+++ froggs.py
@@ -1,22 +1,103 @@
 import numpy as np
+import random
+
+def g(I):
+    if(I.ndim == 2):
+        xx = len(I[0])
+        sum = np.zeros(len(I))
+        for i in range(1, xx):
+            sum = sum + I[:, i] / (xx - 1)
+    if (I.ndim == 1):
+        xx = len(I)
+        sum = np.zeros(1)
+        for i in range(1, xx):
+            sum = sum + I[i] / (xx - 1)
+
+    return 1 + 9 * sum
+    
+
+def f1(I):
+    if(I.ndim == 2):
+        return I[:, 0]
+    if(I.ndim == 1):
+        return I[0]
+
+# ZDT1
+def f2(I):
+    if (I.ndim == 2):
+        return g(I)*(1-I[:,0]/g(I)*I[:,0]/g(I))
+    if (I.ndim == 1):
+        return g(I)*(1-I[0]/g(I)*I[0]/g(I))
+
+
+def Dominates(individual_1, individual_2):
+    # Проверка, что individual_1 и individual_2 - это массивы (сектора)
+    if isinstance(individual_1, np.ndarray) and isinstance(individual_2, np.ndarray):
+        for i in range(len(individual_1)):  # Индексация массивов
+            if individual_1[i] > individual_2[i]:
+                return False
+        return any(val_1 < val_2 for val_1, val_2 in zip(individual_1, individual_2))
+    else:
+        raise ValueError("individual_1 and individual_2 should be numpy arrays")
+
+def non_dom_sort(I, flag):
+    population1 = np.array([])
+    I1 = np.array([])
+
+    # Заполняем список `population` значениями функций f1 и f2
+    population = np.vstack((f1(I), f2(I))).T
+
+    while len(population)>0:
+        c = 0
+
+        for i in range(len(population)):
+            c = 0
+            for j in range(len(population)):
+                if i != j and Dominates(population[j], population[i]):
+                    c += 1
+
+            if c == 0:
+                population1 =np.append(population1,population[i])
+                I1 = np.append(I1, I[i])
+
+
+        # Удаление элементов из `population` и `I`, основываясь на `population1` и `I1`
+        population = population[~np.isin(population, population1)].reshape(-1,2)
+        I = I[~np.isin(I, I1)].reshape(-1,2)
+
+        #print('result', ~np.isin(population, population1))
+        #print(population)
+        # Если флаг установлен, выводим и выходим из функции
+        if flag:
+            return I1.reshape(-1,2)
+    return I1.reshape(-1,2)
 
 def function(x):
-    return (x[0]+2*x[1]-7)**2+(2*x[0]+x[1]-5)**2
+    #return (x[0]+2*x[1]-7)**2+(2*x[0]+x[1]-5)**2
+    #return 100*np.sqrt(abs(x[1]-0.01*x[0]**2))+0.01*abs(x[0]+10)
+    return (x[0]**2+x[1]-11)**2+(x[0]+x[1]**2-7)**2
 
-P = 100
-iters = 50
-it = 10
-M = 5
+P = 200
+iters = 100
+it = 50
+M = 20
 C = 1.6
-a = np.array([-10,-10])
-b = np.array([10,10])
+#a = np.array([-10,-10])
+#b = np.array([10,10])
+#a = np.array([-15, -3])
+#b = np.array([-5, 3])
+a = np.array([0, 0])
+b = np.array([1, 1])
 n = 2
 x = np.random.uniform(a, b, (P,2))
-y = np.zeros(P)
-for i in range (P):
-    y[i] = function(x[i])
-x = x[np.argsort(y)]
-best = x[0]
+# y = np.zeros(P,2)
+# for i in range (P):
+#     y[i,0] = f1(x[i])
+#     y[i,1] = f2(x[i])
+x = non_dom_sort(x,False)
+best = random.choices(non_dom_sort(x,True))[0]
+print(best)
+
 def calculate(worse, best, C, border_0, border_1):  
     sol = border_1+1
     rand = np.random.uniform(0, 1)
@@ -36,23 +117,20 @@ def distribute(x, M):
 
 
 x = distribute(x,M)
-#print(x)
 
 for j in range(iters):
     for i in range(it):
         for group_idx, row in enumerate(x):
-            y_y = np.zeros(len(row))
-            for k in range(len(row)):
-                y_y[k] = function(row[k])
-            # Сортируем строку row
-            row = row[np.argsort(y_y)]
+            
+            row = non_dom_sort(x,False)
             worse = row[-1]
-            best_row = row[0]
-
+            best_row = best = random.choices(non_dom_sort(row,True))[0]
+            
             # Применяем правила обновления
-            if function(calculate(worse, best_row, C, a, b)) < function(worse):
+            #if function(calculate(worse, best_row, C, a, b)) < function(worse):
+            if Dominates(np.array([f1(calculate(worse, best_row, C, a, b)),f2(calculate(worse, best_row, C, a, b))]),np.array([f1(worse),f2(worse)])): 
                 worse = calculate(worse, best_row, C, a, b)
-            elif function(calculate(worse, best, C, a, b)) < function(worse):
+            elif Dominates(np.array([f1(calculate(worse, best, C, a, b)),f2(calculate(worse, best, C, a, b))]),np.array([f1(worse),f2(worse)])):
                 worse = calculate(worse, best, C, a, b)
             else:
                 worse = np.random.uniform(0, 1, 2)
@@ -65,15 +143,12 @@ for j in range(iters):
 
     # Преобразуем массив обратно в одномерный и пересортируем
     x = np.concatenate(x)  # Преобразуем обратно в одномерный массив
-    y = np.zeros(P)
-    for i in range(P):
-        y[i] = function(x[i])
-    x = x[np.argsort(y)]
+    x = non_dom_sort(x,False)
 
     # Обновляем лучшее и худшее решение
-    best = x[0]
-    worse = x[-1]
+    best = random.choices(non_dom_sort(x,True))[0]
+    #worse = x[-1]
 
     # Снова распределяем элементы по группам
     x = distribute(x, M)
-print(best,function(best))
+print(x.reshape(-1,2))
--- parts.py
+++ parts.py
@@ -3,18 +3,23 @@ import numpy as np
 
 
 def function(x):
-    return (x[0]+2*x[1]-7)**2+(2*x[0]+x[1]-5)**2
+    #return (x[0]+2*x[1]-7)**2+(2*x[0]+x[1]-5)**2
+    return 100*np.sqrt(abs(x[1]-0.01*x[0]**2))+0.01*abs(x[0]+10)
+    #return (x[0]**2+x[1]-11)**2+(x[0]+x[1]**2-7)**2
 
 
 NP = 50
 Nmin = 15
 Nmax = 25
-K = 100
+K = 150
 omega = 0.5
 alpha = 0.5
 beta = 0.5
-a = np.array([-10, -10])
-b = np.array([10, 10])
+nstep = 20
+a = np.array([-15, -3])
+b = np.array([-5, 3])
+#a = np.array([-5, -5])
+#b = np.array([5, 5])
 v = np.zeros(NP)
 
 x = np.random.uniform(a, b, (NP, 2))
@@ -39,12 +44,16 @@ for i in range(K):
         vj = omega*v[j]+alpha*(np.random.uniform(0, 1))*(best[j]-x[j]) \
             + omega*beta*(np.random.uniform(0, 1))*(bestj-x[j])
 
-        if function(x[j]+vj) < function(x[j]):
+        bestx = x[j]+vj
+        for k in range (1,nstep+1):
+            if function(bestx) > function(x[j]+vj*(k-nstep//2)-1):
+                bestx = x[j]+vj*(k-nstep//2)-1
+        if function(bestx) < function(x[j]):
             best[j] = x[j]+vj
 
-        x[j] += vj
+        x[j] = bestx
 y = np.zeros(NP)
 
 for i in range (NP):
     y[i] = function(x[i])
-print(x[np.argmin(y)])
+print(x[np.argmin(y)], min(y))
